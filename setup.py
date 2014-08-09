9af7e63ec2d7da3a3a37067204c8f2905daa4512
Rafael Medina <rafamedgar@gmail.com>
2014-08-09 21:58:29 +0200

    Bump to 0.2.1

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
 
 setup(
     name='infocards',
-    version='dev',
+    version='0.2.1',
 
     description='Simple information card archive library',
     long_description=long_description,
