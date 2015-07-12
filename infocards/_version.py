c39c61b1f13cf7b4d95b154a5b141501ae6a3a7b
Rafael Medina <rafamedgar@gmail.com>
2015-07-12 12:46:01 +0200

    Updated version

--- infocards/_version.py
+++ infocards/_version.py
@@ -19,4 +19,4 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-__version__ = '0.3.0'
+__version__ = '0.5.0'
